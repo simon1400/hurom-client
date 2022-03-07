9e2d4c9d0418f86676922a992ac047e98977e15e
Dimmi <pechunka11@gmail.com>
2022-03-07 16:23:48 +0100

    sdfsdf

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '../styles/main.scss'
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import TagManager from 'react-gtm-module';
+import { DataProvider } from '../context/dataStateContext'
+import { useEffect } from 'react';
 // loads the Icon plugin
 UIkit.use(Icons);
 
-import { DataProvider } from '../context/dataStateContext'
-
 const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-WJJLLH9' });
+  }, []);
   return <DataProvider><Component {...pageProps} /></DataProvider>
 }
-
 
 export default App
